3edf5722734b89b4d530d3530bf1831d5306a705
Kerecsényi Márk <karatelovag@gmail.com>
2022-09-22 16:08:09 +0200

    Ember osztályban elkészültek az adattagok

--- src/hu/petrik/emberekoop/Ember.java
+++ src/hu/petrik/emberekoop/Ember.java
@@ -12,6 +12,9 @@ public class Ember {
             this.szulHely = szulHely;
         }
 
+        public int getSzuletesiEv(){
+
+        }
     @Override
     public String toString() {
         return String.format("%30s %10s %-20s", this.nev, this.szulDatum, this.szulHely);
