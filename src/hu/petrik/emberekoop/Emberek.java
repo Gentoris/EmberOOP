00d42ae2616e1b99f2880d0802c8b9bc998b3792
Kerecsényi Márk <karatelovag@gmail.com>
2022-09-22 17:19:55 +0200

    év honap nap

--- src/hu/petrik/emberekoop/Emberek.java
+++ src/hu/petrik/emberekoop/Emberek.java
@@ -1,6 +1,7 @@
 package hu.petrik.emberekoop;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Emberek {
@@ -8,10 +9,22 @@ public class Emberek {
 
     public Emberek(Ember[] emberTomb) {
         this.emberek = new ArrayList<>();
+        this.emberek.addAll(Arrays.asList(emberTomb));
         /*
         for (Ember e: emberTomb) {
             this.emberek.add(e);
         }
         */
     }
-}
+
+        @Override
+        public String toString() {
+        StringBuilder s = new StringBuilder(String.format("%30s %10s (%3s év) %20s",
+                "Név", "Szül.dátum", "kor", "Szül.hely"));
+        for (int i = 0; i < this.emberek.size(); i++) {
+                s.append(System.lineSeparator()).append(this.emberek.get(i));
+            }
+            return s.toString();
+        }
+    }
+
